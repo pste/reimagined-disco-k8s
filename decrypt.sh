40dcc777d5276ca3630bf6264515f925ab6fe0ad
pste <pirraste@gmail.com>
2025-06-14 18:00:55 +0200

    [sops] crypt decrypt with age

--- decrypt.sh
+++ decrypt.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-touch decrypted
 export SOPS_AGE_KEY_FILE=$(pwd)/private/age-key.txt
-sops --decrypt --in-place ./k8s/secrets.yaml
 
+# create lock file
+touch decrypted
 
+SECFILES="secrets secrets-smb secrets-pg"
+
+for SECFILE in $SECFILES; do
+  FNAME="./k8s/$SECFILE.yaml"
+  if [ -f $FNAME ]; then
+    echo "Decrypting $FNAME ..."
+    sops --decrypt --in-place $FNAME
+  fi
+done
--- encrypt.sh
+++ encrypt.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 export SOPS_AGE_RECIPIENTS=$(<public-age-keys.txt)
 
-sops --encrypt --in-place --age ${SOPS_AGE_RECIPIENTS} ./k8s/secrets.yaml
+SECFILES="secrets secrets-smb secrets-pg"
 
+for SECFILE in $SECFILES; do
+  FNAME="./k8s/$SECFILE.yaml"
+  if [ -f $FNAME ]; then
+    echo "Encrypting $FNAME ..."
+    sops --encrypt --in-place --age ${SOPS_AGE_RECIPIENTS} $FNAME
+  fi
+done;
+
+# rm lock file
 rm decrypted
